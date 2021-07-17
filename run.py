f19e54facd53f8a6770aa6abc6820e10e4443d3f
hartnetl <laura.hartnett@outlook.com>
2021-07-17 10:57:20 +0000

    Feat: Begin to add some colour

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 import sys
 import time
+from termcolor import colored, cprint
 
 # These are the APIs needed to access the google sheet data
 SCOPE = [
@@ -23,8 +24,10 @@ new_logger = SHEET.worksheet('log_21')
 logger_20 = SHEET.worksheet('log_20')
 info = SHEET.worksheet('admin')
 
-# data = raw_data.get_all_values()
-# print(data)
+# define text colours
+print_red = lambda x: cprint(x, 'red')
+print_green = lambda x: cprint(x, 'green')
+print_blue = lambda x: cprint(x, 'blue')
 
 
 def welcome():
@@ -55,20 +58,18 @@ def welcome():
 
 # Credit for help with this function
 # https://stackoverflow.com/questions/4099422/printing-slowly-simulate-typing
-def type_print(str):
-    """
-    Prints text out letter by letter instead of all at once
-    """
-#     for letter in str + '\n':
-#         sys.stdout.write(letter)
-#         # sys.stdout.flush()
-#         time.sleep(.8)
-    for char in str:
-        print(char, end='') 
-        sys.stdout.flush() 
-        time.sleep(0.2)
-
-
+# def type_print(str):
+#     """
+#     Prints text out letter by letter instead of all at once
+#     """
+# #     for letter in str + '\n':
+# #         sys.stdout.write(letter)
+# #         # sys.stdout.flush()
+# #         time.sleep(.8)
+#     for char in str:
+#         print(char, end='') 
+#         sys.stdout.flush() 
+#         time.sleep(0.2)
 
 
 def fast_print(str):
@@ -86,19 +87,19 @@ def help():
     """
     Provide help info for users if needed
     """
-    type_print("HELP")
-    type_print('Headings:')
-    type_print("DATE, SPECIES, ID, LOCATION, NEST(Y/N), DATA LOGGER (Y/N)")
-    type_print("For 'DATE' enter the date the turtle or nest was observed in format day/month/year")
-    type_print("For 'SPECIES', enter 'GREEN' for Green Sea Turtle or 'LOG' for Loggerhead Turtle")
-    type_print("For 'ID' enter the id code on the turtle's flipper tag, which is CY followed by 4 digits")
-    type_print("For 'LOCATION' enter B1 or B2 for the beach that you observed the turtle or nest")
-    type_print("For 'NEST' type 'Y' if a nest was successfully laid, or 'N' if the nest and egg laying was not completed")
-    type_print("For 'DATA LOGGER' type 'Y' if a data logger was placed in the nest, and 'N' if it wasn't. Type NA if no nest was laid.")
-    type_print(" ")
-    type_print('Examples:')
-    type_print("01/06/2021, LOG, CY0000, B1, Y, Y")
-    type_print("01/06/2021, GREEN, CY0101, B2, N, NA\n")
+    print_green("HELP")
+    print_green('Headings:')
+    print_green("DATE, SPECIES, ID, LOCATION, NEST(Y/N), DATA LOGGER (Y/N)")
+    print_green("For 'DATE' enter the date the turtle or nest was observed in format day/month/year")
+    print_green("For 'SPECIES', enter 'GREEN' for Green Sea Turtle or 'LOG' for Loggerhead Turtle")
+    print_green("For 'ID' enter the id code on the turtle's flipper tag, which is CY followed by 4 digits")
+    print_green("For 'LOCATION' enter B1 or B2 for the beach that you observed the turtle or nest")
+    print_green("For 'NEST' type 'Y' if a nest was successfully laid, or 'N' if the nest and egg laying was not completed")
+    print_green("For 'DATA LOGGER' type 'Y' if a data logger was placed in the nest, and 'N' if it wasn't. Type NA if no nest was laid.")
+    print(" ")
+    print_green('Examples:')
+    print_green("01/06/2021, LOG, CY0000, B1, Y, Y")
+    print_green("01/06/2021, GREEN, CY0101, B2, N, NA\n")
 
 
 def collect_raw_data():
@@ -106,13 +107,13 @@ def collect_raw_data():
     User inputs the collected raw data.
     """
     # Prompt the user to input the data, or ask for help on how to format it
-    type_print("Enter latest nesting data")
-    type_print("Type 'help' if you need information on the correct format\n")
+    print("Enter latest nesting data")
+    print("Type 'help' if you need information on the correct format\n")
 
-    user_data = input("Enter the data here:\n ")
+    user_data = input("Enter the data here: \n ")
 
     # Return the input data by the user for them to double check it is correct
-    type_print(f"The data you provided here is '{user_data}'\n")
+    print(f"The data you provided here is '{user_data}'\n")
 
     # Call a function to continue if they say yes(Y), or restart if they say no (N)
     check = input("Is this correct? (Y/N) \n")
@@ -132,7 +133,7 @@ def user_verifiy_input(letter):
     if letter == "Y" or letter == "y":
         pass
     else:
-        type_print("Try again\n")
+        print_red("Try again\n")
         collect_raw_data()
 
 
@@ -176,13 +177,13 @@ def user_data_validation(userDataList):
                 f"You must enter 'Y' or 'N' for nest, you entered {userDataList[5]}"
             )
     except ValueError as e:
-        type_print(f"Something went wrong: {e}, try again")
+        print(f"Something went wrong: {e}, try again")
         collect_raw_data()
     except NameError as e:
-        type_print(f"You entered the wrong values: {e}, try again")
+        print(f"You entered the wrong values: {e}, try again")
         collect_raw_data()
     finally:
-        type_print("Validation complete")
+        print("Validation complete")
         send_data_to_worksheets(userDataList)
 
 
@@ -190,19 +191,19 @@ def send_data_to_worksheets(data):
     """
     After validation, the data input by the user is added to the raw data worksheet.
     """
-    type_print("Updating the worksheet...\n")
+    print("Updating the worksheet...\n")
     upper_data = [item.upper() for item in data]
     raw_data.append_row(upper_data)
-    type_print("Raw datasheet update complete!\n")
+    print("Raw datasheet update complete!\n")
 
     if upper_data[1] == "LOG":
         upper_data.remove('LOG')
         new_logger.append_row(upper_data)
-        type_print("Data also sent to log_21 worksheet")
+        print("Data also sent to log_21 worksheet")
     elif upper_data[1] == "GREEN":
         upper_data.remove('GREEN')
         new_green.append_row(upper_data)
-        type_print("Data also sent to green_21 worksheet")
+        print("Data also sent to green_21 worksheet")
 
 
 def calculate_total_nests():
@@ -214,7 +215,7 @@ def calculate_total_nests():
     for item in nest_col:
         if item == "Y":
             total += 1
-    type_print("Calculating total nests")
+    print("Calculating total nests")
     return total
 
 
@@ -224,8 +225,8 @@ def append_total_nests(total):
     """
     info.update('B2', total)
     updated = info.acell('B2').value
-    type_print("Updating total in admin worksheet")
-    type_print(f"{updated} nests have been laid this season so far!")
+    print("Updating total in admin worksheet")
+    print(f"{updated} nests have been laid this season so far!")
 
 
 def calculate_green_and_logger_nests():
@@ -236,28 +237,28 @@ def calculate_green_and_logger_nests():
     # Calculate greens first
     green_nest = new_green.col_values(4)
     green_total = 0
-    type_print("Calculating green nests")
+    print("Calculating green nests")
     for item in green_nest:
         if item == "Y":
             green_total += 1
 
-    type_print("Adding to admin sheet")
+    print("Adding to admin sheet")
     info.update('D2', green_total)
     green = info.acell('D2').value
-    type_print(f"There have been {green} Green Sea Turtle nests laid so far this season")
+    print(f"There have been {green} Green Sea Turtle nests laid so far this season")
 
     # Now the loggerheads
     logger_nest = new_logger.col_values(4)
     logger_total = 0
-    type_print("Calculating logger nests")
+    print("Calculating logger nests")
     for items in logger_nest:
         if items == "Y":
             logger_total += 1
 
-    type_print("Adding to admin sheet")
+    print("Adding to admin sheet")
     info.update('E2', logger_total)
     logger = info.acell('E2').value
-    type_print(f"There have been {logger} Loggerhead Turtle nests laid so far this season")
+    print(f"There have been {logger} Loggerhead Turtle nests laid so far this season")
 
 
 def calculate_data_logger_stock():
@@ -270,12 +271,12 @@ def calculate_data_logger_stock():
     for item in data_logs:
         if item == "Y":
             total -= 1
-    type_print("Calculating data loggers left")
+    print("Calculating data loggers left")
 
     info.update("A2", total)
-    type_print("Updating data log stock value")
+    print("Updating data log stock value")
     updated = info.acell('A2').value
-    type_print(f"You have {updated} data loggers left")
+    print(f"You have {updated} data loggers left")
 
 
 def calculate_difference():
@@ -283,43 +284,43 @@ def calculate_difference():
     Calculate and return the number of green and
     logger turtle nests laid compared to last year
     """
-    type_print("Calculating difference in total nest numbers compared to last year")
+    print("Calculating difference in total nest numbers compared to last year")
     last_total = int(info.acell('C2').value)
     this_total = int(info.acell('B2').value)
     total_diff = last_total - this_total
     if total_diff > 0:
-        type_print(f"There were {total_diff} more nests laid in total last year")
+        print(f"There were {total_diff} more nests laid in total last year")
     elif total_diff < 0:
-        type_print(f"There were {total_diff} less nests laid in total last year")
+        print(f"There were {total_diff} less nests laid in total last year")
     elif total_diff == 0:
-        type_print("The same amount of nests were laid last year")
+        print("The same amount of nests were laid last year")
 
-    type_print("Calculating difference in green turtle nest numbers compared to last year")
+    print("Calculating difference in green turtle nest numbers compared to last year")
     last_green = int(green_20.acell('F2').value)
     this_green = int(info.acell('D2').value)
     green_diff = last_green - this_green
     if green_diff > 0:
-        type_print(f"There were {green_diff} more green nests laid last year")
+        print(f"There were {green_diff} more green nests laid last year")
     elif green_diff < 0:
-        type_print(f"There were {green_diff} less green nests laid last year")
+        print(f"There were {green_diff} less green nests laid last year")
     elif green_diff == 0:
-        type_print("The same amount of nests were laid last year")
+        print("The same amount of nests were laid last year")
 
-    type_print("Calculating difference in loggerhead turtle nest numbers compared to last year")
+    print("Calculating difference in loggerhead turtle nest numbers compared to last year")
     last_loggerhead = int(logger_20.acell('F3').value)
     this_loggerhead = int(info.acell('E2').value)
     loggerhead_diff = last_loggerhead - this_loggerhead
     if loggerhead_diff > 0:
-        type_print(f"There were {loggerhead_diff} more loggerhead nests laid last year")
+        print(f"There were {loggerhead_diff} more loggerhead nests laid last year")
     elif loggerhead_diff < 0:
-        type_print(f"There were {loggerhead_diff} less loggerhead nests laid last year")
+        print(f"There were {loggerhead_diff} less loggerhead nests laid last year")
     elif loggerhead_diff == 0:
-        type_print("The same amount of nests were laid last year")
+        print("The same amount of nests were laid last year")
 
 
 # welcome()
 collect_raw_data()
-# total = calculate_total_nests()
+total = calculate_total_nests()
 # append_total_nests(total)
 # calculate_data_logger_stock()
 # calculate_green_and_logger_nests()
